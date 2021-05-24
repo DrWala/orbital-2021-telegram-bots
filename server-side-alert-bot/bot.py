f4c6a2f046e4646c1e8ded88b7127cd17d660ebd
Azeem Vasanwala <azeem.iron@gmail.com>
2021-05-25 00:22:06 +0800

    Add server side alert bot

---
+++ server-side-alert-bot/bot.py
@@ -0,0 +1,52 @@
+import logging
+import time
+
+import requests
+from telegram import bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+token = "1743148790:AAGZ_VuSpv4DWNqyg9woAJfUGQ61mgQi7C4"
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def manual_poll(updater):
+    while True:
+        r = requests.get("http://localhost:5000")
+        if "Blue" in str(r.content):
+            # we got the file!
+            # exit or wait until next day, use break to escape while loop
+            updater.bot.send_message(chat_id="29777902", text="aha!")
+
+        time.sleep(1)
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(token, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # log all errors
+    dp.add_error_handler(error)
+    manual_poll(updater)
+
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
